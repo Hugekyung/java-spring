b0b73fec8a555899c25a7fc4dc452bd944ac5032
yang <kiki9510@gmail.com>
2021-03-06 02:21:44 +0900

    java JSP and Scope basic

---
+++ firstweb/src/examples/ApplicationScope02.java
@@ -0,0 +1,48 @@
+package examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ApplicationScope02
+ */
+@WebServlet("/ApplicationScope02")
+public class ApplicationScope02 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ApplicationScope02() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+response.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		ServletContext application = getServletContext();
+		try {
+			int value = (int)application.getAttribute("value"); // (int)는 형식을 정수 형태로 변환하는 코드
+			value++; // value + 1
+			application.setAttribute("value", value);
+			
+			out.println("<h1>value: " + value + "</h1>");
+		} catch (NullPointerException e) {
+			out.print("value의 값이 없습니다");
+		}
+	}
+
+}
